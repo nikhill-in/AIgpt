e79c346fdcde758290ef1720bab0d8da3ad2cb22
Nikhil Singh Chauhan <nikhik2233@gmail.com>
2026-06-30 23:18:23 +0530

    backend structure setup and db connection code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import connectMongo from "./src/config/db.connection.js";
 
 const app = express();
 app.use(cors({
@@ -11,3 +12,4 @@ app.use(cookieParser());
 app.listen(5000, ()=>{
     console.log("Running on ",5000);
 });
+connectMongo();
