67d22035a58b0b01fb8a198c47b433bb6e2acae1
Nikhil Singh Chauhan <nikhik2233@gmail.com>
2026-07-20 23:35:26 +0530

    service issue

--- backend/src/service/llama.service.js
+++ backend/src/service/llama.service.js
@@ -59,38 +59,3 @@ export async function chatService(input, instructions = "You are a helpful assis
 //   const data = await response.json();
 //   return data.choices?.[0]?.message?.content || data.error || "no response";
 // }
-
-
-
-
-
-
-
-
-// const HF_TOKEN = process.env.HF_TOKEN;
-// const MODEL = "meta-llama/Meta-Llama-3-8B-Instruct";
-
-// export async function chat(input, instructions = "You are a helpful assistant.") {
-//   const response = await fetch(
-//     "https://router.huggingface.co/v1/chat/completions",
-//     {
-//       method: "POST",
-//       headers: {
-//         Authorization: `Bearer ${HF_TOKEN}`,
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         model: MODEL,
-//         messages: [
-//           { role: "system", content: instructions },
-//           { role: "user", content: input },
-//         ],
-//         max_tokens: 300,
-//         temperature: 0.7,
-//       }),
-//     }
-//   );
-
-//   const data = await response.json();
-//   return data.choices?.[0]?.message?.content || data.error || "no response";
-// }
