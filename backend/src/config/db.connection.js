f1a3dbc5cd9b173707063c653fd8697f6f298091
Nikhil Singh Chauhan <nikhik2233@gmail.com>
2026-07-24 18:51:13 +0530

    updated backend

--- backend/src/config/db.connection.js
+++ backend/src/config/db.connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    console.log("URI: ", process.env.MONGO_URI);
+    // console.log("URI: ", process.env.MONGO_URI);
     await mongoose.connect(
       process.env.MONGO_URI
     );
--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
   },
   token: {
     type: String,
-  },
+  }, 
 });
 
 const User = mongoose.model("User", userSchema);
