58ba0857161478042235cc328349f2a5f27f2c7f
Nikhil Singh Chauhan <nikhik2233@gmail.com>
2026-08-01 21:44:13 +0530

    updated..

--- backend/src/controller/chat.controller.js
+++ backend/src/controller/chat.controller.js
@@ -16,3 +16,15 @@ export const getChatMessages = async (req, res) => {
   const messages = await Message.find({ chat: chat._id }).sort({ createdAt: 1 });
   res.json(messages);
 };
+
+export const deleteChat = async(req,res)=>{
+  const chat = await Chat.findOne({_id: req.params.chatId});
+
+  if(!chat){
+    return res.status(404).json({
+      success: false, message: "Chat not found"
+    })
+  }
+
+  const chats = await Chat.deleteOne({_id: req.params.chatId})
+}
