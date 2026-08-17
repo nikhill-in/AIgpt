0ef47eb73f71357019870692acdb701ac6749ed8
Nikhil Singh Chauhan <nikhik2233@gmail.com>
2026-08-17 23:33:20 +0530

    UI update

--- frontend/src/components/chats/ChatHeader.jsx
+++ frontend/src/components/chats/ChatHeader.jsx
@@ -74,6 +74,23 @@ export default function ChatHeader({ onHistoryClick }) {
                 History
               </button>
               <button
+                onClick={onHistoryClick}
+                className="
+    group               /* Add 'group' here */
+    w-full rounded-lg px-3 py-2 text-left
+    transition-all duration-100 text-sm
+    text-[#1a1a1e] dark:text-[#f5f5f77e]
+    hover:bg-[#eaeaec] dark:hover:bg-[#22222a]
+    hover:border-b-2 hover:border-l border-blue-700
+  "
+              >
+                Get{" "}
+                <span className="transition-all group-hover:text-orange-700 duration-200 group-hover:text-xl group-hover:font-bold">
+                  Pro
+                </span>
+                {/* Change 'hover:' to 'group-hover:' and added 'transition-all duration-200' */}
+              </button>
+              <button
                 onClick={logout}
                 className="
                   w-full rounded-lg px-3 py-2 text-left text-sm
--- frontend/src/components/chats/MessageBubble.jsx
+++ frontend/src/components/chats/MessageBubble.jsx
@@ -70,7 +70,7 @@ export default function MessageBubble({ role, content, createdAt, onEdit }) {
         className={`
           max-w-[75%] rounded-2xl px-4 py-3 text-sm leading-6
           ${isUser
-            ? "bg-gradient-to-br from-[#ff7a18] to-[#ff4d00] text-white"
+            ? "bg-gradient-to-br from-[#183bff] to-[#1e00ff] text-white"
             : "border border-[#e5e5e8] dark:border-[#26262c] bg-white dark:bg-[#141418] text-[#1a1a1e] dark:text-[#f5f5f7]"}
         `}
       >
